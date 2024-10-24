b38ba7473f1fb4d7ce53b0678d3362de74388c96
Thais Silva <tatahcristina13@gmail.com>
2024-10-24 17:14:23 -0300

    componente de adicionar item e deletar item

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IListItems } from '../../interface/IListItems.interface';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [InputAddItemComponent, JsonPipe],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  public addItem = signal(true);
 
+  #setListItems = signal<IListItems[]>(this.#parseItems());
+  public getListItems = this.#setListItems.asReadonly();
+
+  #parseItems(){
+    return JSON.parse(localStorage.getItem('@my-list') || '[]')
+  }
+
+  public getInputAddItem(value: IListItems){
+    localStorage.setItem(
+      '@my-list', JSON.stringify([...this.#setListItems(), value])
+    );
+
+    return this.#setListItems.set(this.#parseItems());
+  }
+
+  public deleteAllItems(){
+    localStorage.removeItem('@my-list');
+    return this.#setListItems.set(this.#parseItems());
+  }
 }
