2af8583d96e7c63b721b3b7c3329bb078a214e3f
Thais Silva <tatahcristina13@gmail.com>
2024-10-24 17:54:42 -0300

    editando itens

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 import { IListItems } from '../../interface/IListItems.interface';
-import { JsonPipe } from '@angular/common';
+import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [InputAddItemComponent, JsonPipe],
+  imports: [InputAddItemComponent, InputListItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -26,6 +26,48 @@ export class ListComponent {
     );
 
     return this.#setListItems.set(this.#parseItems());
+  }
+
+  public listItemsStage(value: 'pending' | 'completed'){
+    return this.getListItems().filter((res: IListItems) => {
+      if(value === 'pending'){
+        return !res.checked;
+      }
+
+      if(value === 'completed'){
+        return res.checked;
+      }
+
+      return res;
+    });
+  }
+
+  public updateItemCheckbox(newItem: { id: string, checked: boolean }){
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter(res => {
+        if(res.id === newItem.id){
+          res.checked = newItem.checked;
+          return res;
+        }
+        return res;
+      });
+      return oldValue;
+    });
+    return localStorage.setItem('@my-list', JSON.stringify(this.#setListItems()));
+  }
+
+  public updateItemText(newItem: { id: string, value: string }){
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter(res => {
+        if(res.id === newItem.id){
+          res.value = newItem.value;
+          return res;
+        }
+        return res;
+      });
+      return oldValue;
+    });
+    return localStorage.setItem('@my-list', JSON.stringify(this.#setListItems()));
   }
 
   public deleteAllItems(){
